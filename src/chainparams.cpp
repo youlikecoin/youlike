a0b4c2a5db173ea1773eeba6f37a602c6649bb7c
Mike Champ <youlikecoin@gmail.com>
2020-12-12 17:35:51 +0100

    header patch

--- src/chainparams.cpp
+++ src/chainparams.cpp
@@ -280,7 +280,7 @@ public:
          * The characters are rarely used upper ASCII, not valid as UTF-8, and produce
          * a large 32-bit integer with any alignment.
          */
-        pchMessageStart[0] = 0x8c;
+        pchMessageStart[0] = 0x8d;
         pchMessageStart[1] = 0x25;
         pchMessageStart[2] = 0xf7;
         pchMessageStart[3] = 0x27;
